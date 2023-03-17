c8c62e7da5e2bbffbf3c869ad548d660828f52f4
Lars Vierbergen <lars.vierbergen@xenit.eu>
2023-03-17 14:01:49 +0100

    Initial commit

---
+++ src/main/java/com/contentgrid/pathfinder/PathfinderApplication.java
@@ -0,0 +1,13 @@
+package com.contentgrid.pathfinder;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PathfinderApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PathfinderApplication.class, args);
+	}
+
+}
